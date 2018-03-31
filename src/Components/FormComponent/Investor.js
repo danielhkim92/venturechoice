623f2889b90879a75f92e3c0700596e58f0031ce
danielhkim92 <danielhkim92@gmail.com>
2018-03-30 23:10:47 -0500

    working on therefactoring the form

---
+++ src/Components/FormComponent/Investor.js
@@ -0,0 +1,22 @@
+				<form>
+					<h5>What does Investment Look Like?</h5>
+						<div>
+							<label>
+								Self-Investor
+								<input type="radio" label="Self-Investor" name="investors"/>
+							</label>
+						</div>
+						<div>
+							<label>
+								Group-Investors
+								<input type="radio" value="Group-Investors" name="investors"/>	
+							</label>
+						</div>
+						<div>
+							<label>
+								Big Player Investors
+								<input type="radio" value="Big Player Investors" name="investors"/>
+
+							</label>
+						</div>
+				</form>
