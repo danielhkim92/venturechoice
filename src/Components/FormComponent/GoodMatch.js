6da14e083e705c9786071e488c43d3f0018846d4
danielhkim92 <danielhkim92@gmail.com>
2018-03-31 23:53:44 -0500

    working on it little by little

---
+++ src/Components/FormComponent/GoodMatch.js
@@ -0,0 +1,16 @@
+				<div>
+						<form>
+							<div>
+								<label>
+								Yes
+									<input type="radio" label="yes" name="goodmatch"/>			
+								</label>
+							</div>
+							<div>
+								<label>
+									No
+									<input type="radio" label="no" name="goodmatch"/>			
+								</label>
+							</div>
+					</form>
+				</div><br/>
